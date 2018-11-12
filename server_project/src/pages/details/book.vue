58e6c403d0d7401a32862361354b340e7b810467
xyxlin888@gmail.com <1348298308@qq.com>
2018-11-12 21:32:26 +0800

    price

--- server_project/src/pages/details/book.vue
+++ server_project/src/pages/details/book.vue
@@ -9,10 +9,10 @@
    </el-input>
   </el-form-item>
   <el-form-item v-if= " films" label="活动价格">
-    <el-select style="margin-left:-2.8rem"  v-model = "films.minimumPrice" placeholder="请选择活动区域">
+    <el-select style="margin-left:-2rem"  v-model = "films.minimumPrice" placeholder="请选择活动区域">
       <el-option label="区域一" value="shanghai"></el-option>
       <el-option label="区域二" value="beijing"></el-option>
-    </el-select>
+    </el-select>&nbsp;&nbsp;(元)
   </el-form-item>
   <el-form-item label="活动时间">
     <el-col :span="11">
